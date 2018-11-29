1f896afa26d848374adc92f1f33eca17afb2d321
Lenty Chang <lentychang@gmail.com>
2018-11-29 15:15:59 +0100

    wrap gazebo add/delete model client into a new server

--- thesis/src/add_model_gazebo.py
+++ thesis/src/add_model_gazebo.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 import rospy, tf
-from gazebo_msgs.srv import DeleteModel, SpawnModel, SpawnModelRequest
+from gazebo_msgs.srv import DeleteModel, DeleteModelRequest, SpawnModel, SpawnModelRequest, DeleteModelResponse, SpawnModelResponse
 from thesis_msgs.srv import MvGrpSrv, MvGrpSrvRequest, MvGrpSrvResponse
 from geometry_msgs.msg import Quaternion, Pose, TransformStamped
 from gazebo_msgs.msg import ModelStates
+from std_srvs.srv import Empty, EmptyRequest, EmptyResponse, Trigger, TriggerResponse
 import os
 import ipdb
 from numpy import ndarray
@@ -14,7 +15,6 @@ from math import pi
 
 
 class GazeboModelCli():
-
     def __init__(self):
         self.urdfDir = "/root/catkin_ws/src/sim_device/thesis/urdf"
         self.model_relPose_list = [[[]],
@@ -40,94 +40,128 @@ class GazeboModelCli():
         rospy.wait_for_service("/gazebo/spawn_urdf_model")
 
         # instances regards to server client
-        self.srvProxy = rospy.ServiceProxy("gazebo/spawn_urdf_model", SpawnModel)
+        self.spawn_model_proxy = rospy.ServiceProxy("gazebo/spawn_urdf_model", SpawnModel)
+        self.delete_model_proxy = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
         self.req = SpawnModelRequest()
         self.req.reference_frame = "world"
         self.req.robot_namespace = ""
 
     def add_one_model(self):
-
-        self.resp = self.srvProxy(self.__getReq())
-        rospy.loginfo("add model to gazebo success: " + str(self.resp.success))
-        if self.resp.success:
-            ipdb.set_trace()
-            info = "Add model: '" + str(self.req.model_name), "' at : " + str(self.req.initial_pose) + "(xyz,quaternion)"
+        req = self.__getReq()
+        if req is not None:
+            tmpResp = self.spawn_model_proxy(req)
+        else:
+            tmpResp = SpawnModelResponse()
+            tmpResp.success = False
+            tmpResp.status_message = "No model in Bin"
+        if tmpResp.success:
+            rospy.loginfo("add model to gazebo success: " + str(tmpResp.success))
+            info = "Add model: '" + str(self.req.model_name), "' at : " + str(self.req.initial_pose) + "(xyz,quaternion) succeed!"
             rospy.loginfo(info)
-        self.added_models.append(self.req.model_name)
+            self.added_models.append(self.req.model_name)
+        else:
+            status_message = "Add model failed"
+            rospy.logwarn(status_message)
+            tmpResp.status_message = status_message + ", " + tmpResp.status_message
+
+        resp = TriggerResponse()
+        resp.success = tmpResp.success
+        resp.message = tmpResp.status_message
+
+        return resp
+
+    def del_one_model(self):
+        if len(self.added_models) > 0:
+            delReq = DeleteModelRequest()
+            delReq.model_name = self.added_models.pop()
+            tmpResp = self.delete_model_proxy(delReq)
+            rospy.loginfo("delete model to gazebo success: " + str(tmpResp.success))
+            if tmpResp.success:
+                info = "Deleted model: " + str(delReq.model_name) + " succeed!"
+                rospy.loginfo(info)
+            else:
+                status_message = "Delete failed due to unknown reason"
+                rospy.logwarn(status_message)
+                tmpResp.status_message = status_message
+
+        else:
+            status_message = "No model to be deleted"
+            rospy.logwarn(status_message)
+            tmpResp = DeleteModelResponse()
+            tmpResp.status_message = status_message
+
+        resp = TriggerResponse()
+        resp.success = tmpResp.success
+        resp.message = tmpResp.status_message
+
+        return resp
 
     def __getReq(self):
-        modelNumber = self.modelsInBin.pop(0)
-        modelName = "lf0640" + str(modelNumber)
-        self.req.model_name = modelName + "_" + str(self.randlist.pop())
-        ipdb.set_trace()
+        if len(self.modelsInBin) > 0:
+            modelNumber = self.modelsInBin.pop(0)
+            modelName = "lf0640" + str(modelNumber)
+            self.req.model_name = modelName + "_" + str(self.randlist.pop())
 
-        if (modelNumber > 5 or modelNumber < 0 or type(modelNumber) != int):
-            rospy.logfatal("Model number must be integer and between [1, 5], check GazeboModelCli.modelsInBin")
-            raise TypeError
+            if (modelNumber > 5 or modelNumber < 0 or type(modelNumber) != int):
+                rospy.logfatal("Model number must be integer and between [1, 5], check GazeboModelCli.modelsInBin")
+                raise TypeError
 
-        # assign urdf file
-        self.req.model_xml = self.modelXmlList[modelNumber - 1]
+            # assign urdf file
+            self.req.model_xml = self.modelXmlList[modelNumber - 1]
 
-        # when there are to many possible pose for a model, randomly choose a pose
-        model_relPose = self.model_relPose_list[modelNumber].pop(random.sample(range(0, len(self.model_relPose_list[modelNumber])), 1)[0])
-        worldPose = [self.startXYZ[i] + model_relPose[i] for i in range(0, 6)]
+            # when there are to many possible pose for a model, randomly choose a pose
+            model_relPose = self.model_relPose_list[modelNumber].pop(random.sample(range(0, len(self.model_relPose_list[modelNumber])), 1)[0])
+            worldPose = [self.startXYZ[i] + model_relPose[i] for i in range(0, 6)]
 
-        # assign pose
-        self.req.initial_pose.position.x = worldPose[0]
-        self.req.initial_pose.position.y = worldPose[1]
-        self.req.initial_pose.position.z = worldPose[2]
-        tf_qFromEuler = tf.transformations.quaternion_from_euler(worldPose[3], worldPose[4], worldPose[5])
-        orient = Quaternion(tf_qFromEuler[0], tf_qFromEuler[1], tf_qFromEuler[2], tf_qFromEuler[3])
-        self.req.initial_pose.orientation = orient
-        return self.req
+            # assign pose
+            self.req.initial_pose.position.x = worldPose[0]
+            self.req.initial_pose.position.y = worldPose[1]
+            self.req.initial_pose.position.z = worldPose[2]
+            tf_qFromEuler = tf.transformations.quaternion_from_euler(worldPose[3], worldPose[4], worldPose[5])
+            orient = Quaternion(tf_qFromEuler[0], tf_qFromEuler[1], tf_qFromEuler[2], tf_qFromEuler[3])
+            self.req.initial_pose.orientation = orient
+            return self.req
+        else:
+            rospy.logwarn("no model in Bin")
+            return None
 
     def srvIsDone(self):
         return True if len(self.modelsInBin) else False
 
 
-class GotoPoseCli():
+gazeboModelCli = GazeboModelCli()
 
-    def __init__(self):
-        rospy.wait_for_service("mvGrpSrv")
-        self.srvProxy = rospy.ServiceProxy("mvGrpSrv", MvGrpSrv)
-        self.req = MvGrpSrvRequest()
-        self.req.poseNumber = 0
 
-    def goToNthPose(self, goToNext=True):
-        if goToNext and self.req.poseNumber <= 1:
-            self.req.poseNumber += 1
-        else:
-            rospy.logwarn("Has tried all defined positions, if no object recognized, please define more position for the robot")
-        self.resp = self.srvProxy(self.req)
-        rospy.loginfo("Go n-th pose success: " + str(self.resp.success))
+def addModelSrvCb(req):
+    return gazeboModelCli.add_one_model()
 
-    def goToFirstPose(self):
-        self.req.poseNumber = 1
-        self.resp = self.srvProxy(self.req)
-        rospy.loginfo("Go 1st pose success: " + str(self.resp.success))
+
+def delModelSrvCb(req):
+    return gazeboModelCli.del_one_model()
+
+
+def delAllModelSrvCb(req):
+    for i in range(0, len(gazeboModelCli.added_models)):
+        gazeboModelCli.del_one_model()
+    resp = TriggerResponse()
+    resp.success = True
+    msg = "All model deleted!"
+    rospy.loginfo(msg)
+    resp.message = msg
+    return resp
 
 
 if __name__ == '__main__':
     # wait for all servers
-    rospy.init_node("spawn_products_in_bins")
+    rospy.init_node("addModelSrv")
+    s = rospy.Service('/addModelSrv', Trigger, addModelSrvCb)
+    d = rospy.Service('/delModelSrv', Trigger, delModelSrvCb)
+    d = rospy.Service('/delAllModelSrv', Trigger, delAllModelSrvCb)
 
-    gazeboModelCli = GazeboModelCli()
-    gotoPoseCli = GotoPoseCli()
-    ipdb.set_trace()
-
-    rospy.loginfo("All Service Server Connected")
-
-    # initialize all clients
-    # delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
-
-    gazeboModelCli.add_one_model()
-    gotoPoseCli.goToFirstPose()
+    rospy.loginfo("Following Server are Connected: /addModelSrv, /delModelSrv")
+    rospy.spin()
 
 
-    raw_input("Press Enter to add next model\n")
-
-
-    ipdb.set_trace()
 
     # raw_input("Press Enter to delete\n")
     # # delete_model(req.model_name)
